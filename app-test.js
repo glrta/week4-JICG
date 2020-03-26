309cfa582e684ee63d43c1a6d1b493ec1755b121
Chloe <chloeh24@hotmail.com>
2020-03-26 17:00:49 +0000

    add tests and check handler Relates #14
    Co-authored-by: Ivo <ievans.storrie@gmail.com>

--- app-test.js
+++ app-test.js
@@ -2,54 +2,54 @@ const test = require('tape');
 const supertest = require('supertest')
 const router = require('./router')
 
-// test("initialise", t => {
-//     let x = 5
-//     t.equal(x, 5, 'Should return 5')
-//     t.end()
-// });
+test("initialise", t => {
+    let x = 5
+    t.equal(x, 5, 'Should return 5')
+    t.end()
+});
 
 
-// test('Home route returns a status code of 200', t => {
-//     supertest(router)
-//         .get('/')
-//         .expect(200)
-//         .expect('content-type', 'text/html')
-//         .end((err, res) => {
-//             t.error(err);
-//             t.match(res.text, /<!DOCTYPE html>/)
-//             t.end()
-//         })
-// })
-
-
-// test('Expected 404 not found error', t =>{
-//   supertest(router)
-//     .get('/something-nonsense')
-//     .expect(404)
-//     .expect('content-type', 'text/html')
-//     .end((err, res) => {
-//       t.error(err)
-//       t.match(res.text, /<h2>Whoops<\/h2>/)
-//       t.end()
-//     })
-    
-//     })
-
-      test('can send post request to blog server', t => {
-        supertest(router)
-          .post('/write')
-          .send('name=Giovanna&title=my20%title')
-          .set('Accept', 'application/json')
-          .expect(200)
-        //   .expect('content-type', 'application/x-www-form-urlencoded')
-          .end((err, res) => {
-            console.log(res.text)
+test('Home route returns a status code of 200', t => {
+    supertest(router)
+        .get('/')
+        .expect(200)
+        .expect('content-type', 'text/html')
+        .end((err, res) => {
             t.error(err);
-            t.equal(res.text.name, 'Giovanna')
-            // t.deepEqual(actual, expected);      
-            t.end();
-          })
+            t.match(res.text, /<!DOCTYPE html>/)
+            t.end()
+        })
+})
+
+
+test('Expected 404 not found error', t =>{
+  supertest(router)
+    .get('/something-nonsense')
+    .expect(404)
+    .expect('content-type', 'text/html')
+    .end((err, res) => {
+      t.error(err)
+      t.match(res.text, /<h2>Whoops<\/h2>/)
+      t.end()
     })
+    
+    })
+
+    //   test('can send post request to blog server', t => {
+    //     supertest(router)
+    //       .post('/write')
+    //       .send('name=Giovanna&title=my20%title')
+    //       .set('Accept', 'application/json')
+    //       .expect(302)
+    //       .expect('content-type', 'application/x-www-form-urlencoded')
+    //       .end((err, res) => {
+    //         console.log(res.text)
+    //         t.error(err);
+    //         t.equal(res.text.name, 'Giovanna')
+    //         t.deepEqual(actual, expected);      
+    //         t.end();
+    //       })
+    // })
 
 
       // describe('POST /user', function() {
