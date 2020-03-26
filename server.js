6a45681da183e0f7e62226df92ee1eccb44f8fe6
Ivo Evans <ievans.storrie@gmail.com>
2020-03-26 12:19:13 +0000

    Change host name var for test Relates #16
    Co-authored-by: Jack <jackherizsmith@gmail.com>

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const router = require("./router");
 
-const host = process.env.HOST || "localhost";
+const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 3001;
 
-http.createServer(router).listen(port, host, () => {
+http.createServer(router).listen(port, () => {
   console.log(`Listening on ${host}:${port}`);
 });
