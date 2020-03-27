e99e344086282e900bc682e0cfcf36e9e42cabdf
Ivo Evans <ievans.storrie@gmail.com>
2020-03-27 11:27:47 +0000

    All inputs are required. Closes #39
    Co-authored-by: Chloe <chloeh24@hotmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const router = require("./router");
 
-const host = process.env.HOST || "0.0.0.0";
+// const host = process.env.HOST || "localhost"; // testing
+const host = process.env.HOST || "0.0.0.0"; // deployment
 const port = process.env.PORT || 3001;
 
 http.createServer(router).listen(port, () => {
